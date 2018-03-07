5c1a4b90788349a605f7120d87a50e90899d2c86
surbhi <surbhi.henceforth@gmail.com>
2018-03-07 06:56:55 +0530

    location

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ request({
   json: true
 }, (err, res, body) =>{
   //the 3rd argument specify the no. of spaces for indentation
-   console.log(JSON.stringify(body, undefined, 2));
+   //console.log(JSON.stringify(body, undefined, 2));
+   console.log(`Address: ${body.results[0].formatted_address}`);
+   console.log(`latitude: ${body.results[0].geometry.location.lat}`);
+   console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+
 });
